7aefd058b70b5d9acaf38e00812e181a4d4cd844
Huỳnh Phúc <huynhngochoangphuc@gmail.com>
2020-04-20 11:22:59 +0700

    refactor: Replace text with enum

--- app/Domain/Support/BasePolicy.php
+++ app/Domain/Support/BasePolicy.php
@@ -4,16 +4,18 @@
 namespace App\Domain\Support;
 
 
-use App\Domain\Posts\Models\Post;
+use App\Domain\Auth\Enums\UserType;
 use App\Domain\Support\Interfaces\AuthInterface;
 
 class BasePolicy
 {
-    public function before(AuthInterface $user) {
-        return $user->hasRole('super-admin');
+    public function before(AuthInterface $user)
+    {
+        return $user->hasRole(UserType::SUPER_ADMIN);
     }
 
-    public function author(AuthInterface $user, String $permission){
-        return $user->roles->first()->hasPermissionTo($permission);
+    public function author(AuthInterface $user, string $permission)
+    {
+        return $user->hasPermissionTo($permission);
     }
 }
